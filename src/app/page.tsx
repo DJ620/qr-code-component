9fbe4b37544d135fd9f49611d9c23a0b22af23f8
DJ Hersh <DanielJHersh@gmail.com>
2024-03-22 11:56:39 -0700

    fixing image issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
       <div className="w-80 h-[497px] bg-white rounded-2xl p-4 text-center shadow-sm">
-        <Image src={qrCode} alt="QR Code" className="h-72 w-72 rounded-xl" />
+        <Image src="./assets/image-qr-code.png" alt="QR Code" className="h-72 w-72 rounded-xl" />
         <div className="border-black border w-72 h-32 px-3 mt-6">
           <strong className="text-xl text-dark-blue">
             Improve your front-end skills by building projects
